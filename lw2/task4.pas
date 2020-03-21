31f2a26df03020ba633f1151529d391ec02f8ab6
gtfodmen <mishag@yandex.ru>
2020-03-21 07:02:30 +0300

    Create task4.pas

---
+++ lw2/task4.pas
@@ -0,0 +1,29 @@
+﻿PROGRAM WorkWithQueryString(INPUT, OUTPUT);
+USES
+  GPC;
+FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+VAR 
+  KeyCheck, QueryString : STRING;
+BEGIN
+  KeyCheck := '';
+  QueryString := GetEnv('QUERY_STRING');
+  KeyCheck := Copy(QueryString, pos(key, QueryString), length(key));
+  IF Key = KeyCheck
+  THEN                                                                           
+    BEGIN
+      Delete(QueryString, 1, pos(key, QueryString) + length(key));
+      IF (pos('&', QueryString) > 1)                 
+      THEN
+        Delete(QueryString, pos('&', QueryString), length(QueryString));
+      GetQueryStringParameter := QueryString;
+    END
+  ELSE
+    GetQueryStringParameter := 'No Request'
+END;    
+BEGIN {WorkWithQueryString}
+  WRITELN;
+  WRITELN('First Name: ', GetQueryStringParameter('first_name'));
+  WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
+  WRITELN('Age: ', GetQueryStringParameter('age'))
+END. {WorkWithQueryString}
+
